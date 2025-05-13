2e5515a6189be269aedac77547184bb6f756a173
Bryce Sharman <bryce.sharman@toronto.ca>
2025-05-13 10:47:30 -0400

    Added summarize_trips_by_mode to MicroSim class.

--- src/gtamodel_tools/microsim/microsim.py
+++ src/gtamodel_tools/microsim/microsim.py
@@ -1,57 +1,33 @@
-""" """
 from os import PathLike
 import pandas as pd
 from pathlib import Path
-
+from typing import Optional, Type
 
 from gtamodel_tools import ModelVersion
-
+import gtamodel_tools.common.data as data
+import gtamodel_tools.common.spatial_aggregator as sa
 
 
 class MicroSim():
-    """ Class to store microsim data.
+    """ Class to store and summarize MicroSimulation results.
 
-    It is expected that this class is instantiated from microsim_io.read_microsim.
+    Args:
+        model_output_dir: os.PathLike
+            Root directory of the model output.
+        model_version: gtamodel_tools.ModelVersion
+            GTA model version.
 
-    Parameters
-    ----------
-    fp: pathlib.Path or str
-        Filepath to directory containing the microsim data.
-
-    Attributes
-    ----------
-    households: pandas.DataFrame
-    persons: pandas.DataFrame
-    trips: pandas.DataFrame
-    trip_modes: pandas.DataFrame
-    trip_stations: pandas.DataFrame
-    facilitate_passenger: pandas.DataFrame
-
-
-    Methods
-    -------
+    Attributes:
 
 
 
+    Methods:
 
-    summarize_households (values_field, attribute_filter, spatial_aggregation)
-    summarize_persons (activity_field, values_field, attribute_filter, spatial_aggregation)
-    summarize_persons_od (activity_field, values_field, attribute_filter, orig_spatial_aggregation, dest_spatial_aggregation)
-    summarize_trips (activity_field, values_field, attribute_filter, orig_spatial_aggregation, dest_spatial_aggregation)
-    summarize_trip_modesplits (activity_field, values_field, attribute_filter, orig_spatial_aggregation, dest_spatial_aggregation)
+
 
     """
     def __init__(
             self, model_output_dir: PathLike, model_version: ModelVersion):
-        """ Initializes MicroSim results
-        
-        Args:
-            model_output_dir: os.PathLike
-                Root directory of the model output.
-            model_version: gtamodel_tools.ModelVersion
-                GTA model version.
-
-        """
         self._model_output_dir = Path(model_output_dir)
         self._model_version = model_version
         self._set_gtamodel_enums()      
@@ -142,6 +118,111 @@ class MicroSim():
             dtype=self._facpac_dtypes, 
             index_col=self._facpac_index_cols
         )
+
+    def summarize_trips_by_mode(
+            self,
+            origin_sa: Type[sa.SpatialAggregator],
+            destination_sa: Type[sa.SpatialAggregator],
+            time_period: Optional[str]=None,
+            households_filter_expr: Optional[str]=None,
+            persons_filter_expr: Optional[str]=None,
+            trips_filter_expr: Optional[str]=None,
+            tripmodes_filter_expr: Optional[str]=None
+            ) -> pd.DataFrame:
+        """ Spatially aggregates trips by mode, allowing optional filters. 
+    
+            Args:
+            origin_sa: subclass of 
+                    gtamodel_tools.common.spatial_aggregator.SpatialAggregator
+                Spatial aggregation for trip origins
+            destination_sa: subclass of 
+                    gtamodel_tools.common.spatial_aggregator.SpatialAggregator
+                Spatial aggregation for trip destinations
+            time_period: str or None:
+                Optional filter for time period. If defined (is not None)
+                then must be one of ['AM', 'MD', 'PM', 'EE', 'LE']
+            households_filter_expr: str or None
+                Optional filter expression on households table. If defined
+                this is applies attribute filter using pandas.eval.
+            persons_filter_expr: str or None
+                Optional filter expression on households table. If defined
+                this is applies attribute filter using pandas.eval.
+            trips_filter_expr: str or None
+                Optional filter expression on households table. If defined
+                this is applies attribute filter using pandas.eval.
+            tripmodes_filter_expr: str or None
+                Optional filter expression on trip_modes table. If defined
+                this is applies attribute filter using pandas.eval. Note that
+                the 'time_period' argument is the preferred way of 
+                identifying trips by default GTAModel time periods.
+    
+            Notes:
+                In order to save memory, the MicroSim class does not merge   
+                all data together into a single DataTable. This method applies 
+                independent filters of the households, persons and trips 
+                tables. 
+
+        """
+        if time_period is not None:
+            if time_period not in self._time_period_definitions.keys():
+                raise AttributeError(
+                    "Invalid time period. If defined, must be one of: "
+                    f"{list(self._time_period_definitions.keys())}")
+            tripmodes_filter_expr = self._apply_timeperiod_filter(
+                self, time_period, tripmodes_filter_expr)
+
+        # We always have to merge trips and tripmodes
+        trips = data.apply_dataframe_filter(
+            self._trips, trips_filter_expr)
+        tripmodes = data.apply_dataframe_filter(
+            self._tripmodes, tripmodes_filter_expr)
+        trips_with_modes = tripmodes.reset_index().merge(
+            trips, 
+            how='inner',
+            left_on=[
+                self._tripmodes_cols['hhld_id'], 
+                self._tripmodes_cols['pers_id'], 
+                self._tripmodes_cols['trip_id']
+            ],
+            right_index=True
+        )
+
+        # If a households or persons filter is applied, filter table and merge  
+        # into tripmodes table.
+        if isinstance(households_filter_expr, str):
+            households_df = data.apply_dataframe_filter(
+                self._households, households_filter_expr)
+            fltr = trips_with_modes[
+                self._tripmodes_cols['hhld_id']].isin(households_df.index)
+            trips_with_modes = trips_with_modes.loc[fltr]
+        if isinstance(persons_filter_expr, str):
+            persons_df = data.apply_dataframe_filter(
+                self._persons, persons_filter_expr).reset_index()
+            fltr = (trips_with_modes[self._tripmodes_cols['hhld_id']].isin(
+                        persons_df[self._pers_cols['hhld_id']])) & (
+                    trips_with_modes[self._tripmodes_cols['pers_id']].isin(
+                        persons_df[self._pers_cols['pers_id']]))
+            trips_with_modes = trips_with_modes.loc[fltr]
+        
+        # Calculate the total weight, and produce summary
+        final_weight_col = 'FINALWEIGHT'
+        if self._trip_cols['weight'] == self._tripmodes_cols['weight']:
+            weight_col = self._tripmodes_cols['weight']
+            trips_with_modes[final_weight_col] = \
+                trips_with_modes[weight_col + '_x'] \
+                * trips_with_modes[weight_col + '_y']
+        else:
+            trips_with_modes[final_weight_col] = \
+                trips_with_modes[self._trip_cols['weight']] \
+                * trips_with_modes[self._tripmodes_cols['weight']]
+        s = sa.summarize_table_with_spatial_aggregation(
+            df=trips_with_modes, 
+            values=final_weight_col, 
+            geom_id=[self._trip_cols['ozone'], self._trip_cols['dzone']],
+            spatial_aggregations=[origin_sa, destination_sa], 
+        ).squeeze() / self._n_tripmode_samples
+        s.index.names = [origin_sa.name, destination_sa.name]
+        return s
 
     def _apply_timeperiod_filter(
             self, time_period: str, tripmodes_filter_expr: str | None) -> str:
